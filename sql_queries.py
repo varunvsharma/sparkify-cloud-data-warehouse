2f441067abe2fdb35d89d1fe383584e70f10974c
Varun Sharma <varunvsharma@gmail.com>
2019-08-13 21:37:08 +1000

    Set up SQL queries for dropping tables

--- sql_queries.py
+++ sql_queries.py
@@ -10,3 +10,21 @@ ARN = config.get('IAM_ROLE', 'ARN')
 LOG_DATA = config.get('S3', 'LOG_DATA')
 LOG_JSONPATH = config.get('S3', 'LOG_JSONPATH')
 SONG_DATA = config.get('S3', 'SONG_DATA')
+
+# DROP TABLES
+
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs"
+songplay_table_drop = "DROP TABLE IF EXISTS songplay"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS song"
+artist_table_drop = "DROP TABLE IF EXISTS artist"
+time_table_drop = "DROP TABLE IF EXISTS time"
+
+drop_table_queries = [staging_events_table_drop,
+                      staging_songs_table_drop,
+                      songplay_table_drop,
+                      user_table_drop,
+                      song_table_drop,
+                      artist_table_drop,
+                      time_table_drop]
