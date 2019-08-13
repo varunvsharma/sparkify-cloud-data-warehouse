a8f3d4bf6eab894d11bfc8f4275a752bf103b7ec
Varun Sharma <varunvsharma@gmail.com>
2019-08-13 21:49:10 +1000

    Set up SQL queries for creating star schema tables

--- sql_queries.py
+++ sql_queries.py
@@ -21,14 +21,6 @@ song_table_drop = "DROP TABLE IF EXISTS song"
 artist_table_drop = "DROP TABLE IF EXISTS artist"
 time_table_drop = "DROP TABLE IF EXISTS time"
 
-drop_table_queries = [staging_events_table_drop,
-                      staging_songs_table_drop,
-                      songplay_table_drop,
-                      user_table_drop,
-                      song_table_drop,
-                      artist_table_drop,
-                      time_table_drop]
-
 # CREATE TABLES
 
 staging_events_table_create= """CREATE TABLE IF NOT EXISTS staging_events (artist varchar,
@@ -60,3 +52,65 @@ staging_songs_table_create = """CREATE TABLE IF NOT EXISTS staging_songs (num_so
                                                                           title varchar,
                                                                           duration float,
                                                                           year int)"""
+
+songplay_table_create = """CREATE TABLE IF NOT EXISTS songplay (songplay_id int IDENTITY(0,1),
+                                                                start_time timestamp NOT NULL,
+                                                                user_id int NOT NULL,
+                                                                level varchar,
+                                                                song_id varchar(18) NOT NULL DISTKEY,
+                                                                artist_id varchar(18) NOT NULL,
+                                                                session_id int,
+                                                                location varchar,
+                                                                user_agent varchar,
+                                                                PRIMARY KEY (songplay_id))
+                            DISTSTYLE KEY"""
+
+user_table_create = """CREATE TABLE IF NOT EXISTS users (user_id int,
+                                                        first_name varchar,
+                                                        last_name varchar,
+                                                        gender varchar,
+                                                        level varchar,
+                                                        PRIMARY KEY (user_id))
+                       DISTSTYLE AUTO"""
+
+song_table_create = """CREATE TABLE IF NOT EXISTS songs (song_id varchar(18) DISTKEY,
+                                                        title varchar,
+                                                        artist_id varchar(18) NOT NULL,
+                                                        year int,
+                                                        duration float,
+                                                        PRIMARY KEY (song_id))
+                       DISTSTYLE KEY"""
+
+artist_table_create = """CREATE TABLE IF NOT EXISTS artists (artist_id varchar(18),
+                                                            name varchar,
+                                                            location varchar,
+                                                            latitude float,
+                                                            longitude float,
+                                                            PRIMARY KEY (artist_id))
+                         DISTSTYLE AUTO"""
+
+time_table_create = """CREATE TABLE IF NOT EXISTS time (start_time timestamp,
+                                                        hour int,
+                                                        day int,
+                                                        week int,
+                                                        month int,
+                                                        year int,
+                                                        weekday int,
+                                                        PRIMARY KEY (start_time))
+                        DISTSTYLE AUTO"""
+
+drop_table_queries = [staging_events_table_drop,
+                      staging_songs_table_drop,
+                      songplay_table_drop,
+                      user_table_drop,
+                      song_table_drop,
+                      artist_table_drop,
+                      time_table_drop]
+
+create_table_queries = [staging_events_table_create,
+                        staging_songs_table_create,
+                        songplay_table_create,
+                        user_table_create,
+                        song_table_create,
+                        artist_table_create,
+                        time_table_create]
