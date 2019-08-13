cc8dbc2134fb7fa104258723c87b6a2529727366
Varun Sharma <varunvsharma@gmail.com>
2019-08-13 22:06:55 +1000

    Add function to load data in staging tables

--- etl.py
+++ etl.py
@@ -1,5 +1,19 @@
 import configparser
 import psycopg2
+from sql_queries import copy_table_queries
+
+
+def load_staging_tables(cur, conn):
+    """Copy data from S3 and load into staging tables in Amazon Redshift.
+
+    Keyword arguments:
+    cur -- Database cursor object
+    conn -- Database connection object
+    """
+    for query in copy_table_queries:
+        cur.execute(query)
+        conn.commit()
+        
 
 def main():
     config = configparser.ConfigParser()
@@ -7,6 +21,8 @@ def main():
 
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
+
+    load_staging_tables(cur, conn)
 
     conn.close()
 
